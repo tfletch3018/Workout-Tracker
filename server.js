16ea09ce298cce43bd125c242e554c04792c1e95
Tonya Fletcher <tfletch4504@gmail.com>
2021-04-22 13:27:28 -0400

    path

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
 });
 
 // routes
-// app.use(require("./routes/api.js"));
-// app.use(require("./routes/view.js"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
