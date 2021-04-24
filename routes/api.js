2489cf59cfc388ce490ed2fe3a037a339c49edd9
Tonya Fletcher <tfletch4504@gmail.com>
2021-04-24 19:04:01 -0400

    correct distance

--- routes/api.js
+++ routes/api.js
@@ -2,35 +2,59 @@ const express = require("express");
 const router = require("express").Router();
 const db = require("../models");
 
-//need to get aggregate method working with workout(addfields and sum operator)
-router.get("/api/workouts", (req, res) => {
-    db.Workout.find({}).then(dbWorkout => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
-});
-
-
-//need to get aggregate method working with workout(addfields and sum operator)
-router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-    .limit(10)
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
-    })
-    .catch((err) => {
-        res.json(err);
-    });
-});
-
-// router.get("/api/workouts/range", ({}, res) => {
-//     db.Workout.find({}).then((dbWorkout) => {
+// router.get("/api/workouts", (req, res) => {
+//     db.Workout.find({}).then(dbWorkout => {
 //         res.json(dbWorkout);
 //     }).catch(err => {
 //         res.status(400).json(err);
 //     });
+//need to get aggregate method working with workout(addfields and sum operator)
+router.get("/api/workouts", ({ body }, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+//need to get aggregate method working with workout(addfields and sum operator)
+// router.get("/api/workouts/range", (req, res) => {
+//     db.Workout.find({})
+//         .limit(10)
+//         .then((dbWorkout) => {
+//             res.json(dbWorkout);
+//         })
+//         .catch((err) => {
+//             res.json(err);
+//         });
 // });
+//need to get aggregate method working with workout(addfields and sum operator)
+router.get("/api/workouts/range", ({ body }, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+                totalWeight: { $sum: "$exercises.weight" },
+                totalSets: { $sum: "$exercises.sets" },
+                totalReps: { $sum: "$exercises.reps" },
+                totalDistance: { $sum: "$exercises.distance" }
+            }
+        }
+    ])
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+})
 
 // router.post("/api/workouts", (req, res) => {
 //     db.Workout.create().then((dbWorkout) => {
@@ -39,22 +63,18 @@ router.get("/api/workouts/range", (req, res) => {
 //         res.status(400).json(err);
 //     });
 // });
-
 router.post("/api/workouts", (req, res) => {
     db.Workout.create({
         day: Date.now()
     })
-    .then(newWorkout => {
-        console.log("Hello I am a new workout: ", newWorkout);
-        res.json(newWorkout);
-    })
+        .then(newWorkout => {
+            console.log("Hello I am a new workout: ", newWorkout);
+            res.json(newWorkout);
+        })
         .catch(err => {
             res.status(400).json(err);
         });
 });
-
-
-
 
 router.put("/api/workouts/:id", (req, res) => {
     db.Workout.findByIdAndUpdate(
@@ -66,6 +86,6 @@ router.put("/api/workouts/:id", (req, res) => {
         .catch(err => {
             res.status(404).json(err);
         });
-    });
+});
 
 module.exports = router;
